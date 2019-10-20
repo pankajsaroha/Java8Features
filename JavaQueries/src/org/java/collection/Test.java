f59d6b9c3b461936f351bad870fc9321af9c8df4
Pankaj Kumar <Pankaj Kumar pk89651@gmail.com>
2019-10-20 07:37:01 +0530

    Custom Hashmap Test

--- JavaQueries/src/org/java/collection/Test.java
+++ JavaQueries/src/org/java/collection/Test.java
@@ -1,8 +1,20 @@
 package org.java.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test{
 
      public static void main(String args[]) throws Exception{
-        System.out.println(Class.forName("String").getName());
+        //System.out.println(Class.forName("String").getName());
+    	 Hashmap<Integer, String> map = new Hashmap<>();
+    	 map.put(11, "Pankaj");
+		 map.put(16, "Saideep");
+		 map.put(2, "Anmol");
+		 map.put(3, "Divya");
+    	 map.put(1, "Nupur");
+    	 System.out.println(map.get(2));
+    	 Map<Integer, String> mp = new HashMap<>();
+    	 System.out.println(mp.get(1));
      }
 }
